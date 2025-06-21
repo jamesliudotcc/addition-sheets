c3fc0c18ee49a9310db9b44e9ad876ccaeaa4f7d
James Liu <james@jamesliu.cc>
2025-06-21 12:52:09 -0700

    Cleanup multiplication CSS

--- src/components/Multiplication.tsx
+++ src/components/Multiplication.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { MULTIPLICATION_SIGN } from '../constants';
 import LatticeSVG from './LatticeSVG';
+import styles from './Multiplication.module.css';
 
 export interface MultiplicationProps {
   firstRow: string;
@@ -18,15 +19,15 @@ const Multiplication: React.FC<MultiplicationProps> = ({
   const firstDigits = firstRow.split('');
   
   return (
-    <article className="problem multiplication-problem">
-      <div className="multiplication-grid">
+    <article className={`problem ${styles.problem}`}>
+      <div className={styles.grid}>
         {/* Top row: First multiplicand digits */}
-        <div className="grid-cell digit-cell" style={{gridArea: 'd1'}}>{firstDigits[0] || ''}</div>
-        <div className="grid-cell digit-cell" style={{gridArea: 'd2'}}>{firstDigits[1] || ''}</div>
-        <div className="grid-cell digit-cell" style={{gridArea: 'd3'}}>{firstDigits[2] || ''}</div>
+        <div className={`${styles.gridCell} ${styles.digitCell}`} style={{gridArea: 'd1'}}>{firstDigits[0] || ''}</div>
+        <div className={`${styles.gridCell} ${styles.digitCell}`} style={{gridArea: 'd2'}}>{firstDigits[1] || ''}</div>
+        <div className={`${styles.gridCell} ${styles.digitCell}`} style={{gridArea: 'd3'}}>{firstDigits[2] || ''}</div>
         
         {/* Lattice grid spans 3x3 in lower left */}
-        <div className="grid-cell lattice-cell" style={{gridArea: 'lat'}}>
+        <div className={`${styles.gridCell} ${styles.latticeCell}`} style={{gridArea: 'lat'}}>
           <LatticeSVG 
             cellSize={50} 
             strokeWidth={1.5}
@@ -34,9 +35,9 @@ const Multiplication: React.FC<MultiplicationProps> = ({
         </div>
         
         {/* Right column: Second multiplicand digits */}
-        <div className="grid-cell digit-cell" style={{gridArea: 'd4'}}>{secondDigits[0] || ''}</div>
-        <div className="grid-cell digit-cell" style={{gridArea: 'd5'}}>{secondDigits[1] || ''}</div>
-        <div className="grid-cell digit-cell" style={{gridArea: 'd6'}}>{secondDigits[2] || ''}</div>
+        <div className={`${styles.gridCell} ${styles.digitCell}`} style={{gridArea: 'd4'}}>{secondDigits[0] || ''}</div>
+        <div className={`${styles.gridCell} ${styles.digitCell}`} style={{gridArea: 'd5'}}>{secondDigits[1] || ''}</div>
+        <div className={`${styles.gridCell} ${styles.digitCell}`} style={{gridArea: 'd6'}}>{secondDigits[2] || ''}</div>
       </div>
     </article>
   );
